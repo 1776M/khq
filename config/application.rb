a5587640429ff6eb6ad76e66e2ad41986a8536de
root <root@plato.kherahq.com>
2012-04-03 20:31:18 +0100

    matrices added to libs

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'matrix'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -17,6 +18,9 @@ module Khq
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+      config.autoload_paths << "#{::Rails.root.to_s}/lib" # <-- set path
+      require "extend_matrix" # <-- forcibly load your matrix extension
+      require "extend_math" # <-- forcibly load your math extension
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
